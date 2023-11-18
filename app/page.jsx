96b1eeee055ecfde6cc337d0ba67e0011b48b3d9
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2023-11-19 01:20:19 +0300

    landing-page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
-import Link from "next/link";
+/* import Image from "next/image";
+import Link from "next/link"; */
 import SplineContainer from "./spline";
-import Testimonials from "./testimonials";
+import Testimonials from "./Testimonials";
 
 export default function Home() {
   return (
